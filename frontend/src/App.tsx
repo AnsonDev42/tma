c5a5239e26473bcb94b928908af2786729ae8939
Han Hwang Lim <hanhwanglim@gmail.com>
2024-05-06 16:19:19 +0100

    Improve validation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,11 @@
 import { useForm } from "react-hook-form";
 import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
 } from "./components/ui/form";
 import { Input } from "./components/ui/input";
 import "./globals.css";
@@ -15,77 +15,78 @@ import { Button } from "./components/ui/button";
 
 const MAX_FILE_SIZE = 25 * 1024 * 1024; // 25MB
 const ACCEPTED_IMAGE_TYPES = [
-  "image/jpeg",
-  "image/jpg",
-  "image/png",
-  "image/webp",
+	"image/jpeg",
+	"image/jpg",
+	"image/png",
+	"image/webp",
 ];
 
 const formSchema = z.object({
-  image: z
-    .any()
-    .refine((file) => file?.size <= MAX_FILE_SIZE, "Max image size is 25MB.")
-    .refine(
-      (file) => ACCEPTED_IMAGE_TYPES.includes(file?.type),
-      "Only .jpg, .jpeg, .png and .webp formats are supported."
-    ),
+	file: z
+		.instanceof(FileList)
+		.refine((fileList) => fileList.length > 0, "Image is required.")
+		.refine(
+			(fileList) => fileList.length <= 1,
+			"Cannot upload more than 1 image",
+		)
+		.refine((file) => file[0].size <= MAX_FILE_SIZE, "Max image size is 25MB.")
+		.refine(
+			(file) => ACCEPTED_IMAGE_TYPES.includes(file[0].type),
+			"Only .jpg, .jpeg, .png and .webp formats are supported.",
+		),
 });
 
 function App() {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      image: "",
-    },
-  });
-  const onSubmit = async (payload: z.infer<typeof formSchema>) => {
-    const formData = new FormData();
-    formData.append("image", payload.image);
+	const form = useForm<z.infer<typeof formSchema>>({
+		resolver: zodResolver(formSchema),
+		defaultValues: {
+			file: undefined,
+		},
+		mode: "onChange",
+	});
 
-    const response = await fetch("http://localhost:8000/upload", {
-      method: "POST",
-      body: formData,
-    });
+	const onSubmit = async (payload: z.infer<typeof formSchema>) => {
+		const formData = new FormData();
+		formData.append("file", payload.file[0]);
+		formData.append("file_name", payload.file[0].name);
 
-    if (!response.ok) {
-      console.error("Failed to send");
-    } else {
-      console.log("Send succesfully");
-    }
-  };
+		const response = await fetch("http://localhost:8000/upload", {
+			method: "POST",
+			body: formData,
+		});
 
-  return (
-    <div className="w-full justify-center flex">
-      <div className="min-w-screen-xl">
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-            <FormField
-              control={form.control}
-              name="image"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Upload an image</FormLabel>
-                  <FormControl>
-                    <Input
-                      id="image"
-                      type="file"
-                      {...field}
-                      accept="image/*"
-                      name="image"
-                      required
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <Button type="submit">Submit</Button>
-          </form>
-        </Form>
-        <div className="grid w-full max-w-sm items-center gap-1.5"></div>
-      </div>
-    </div>
-  );
+		if (!response.ok) {
+			console.error("Failed to send");
+		} else {
+			console.log("Send succesfully");
+		}
+	};
+
+	return (
+		<div className="w-full justify-center flex">
+			<div className="min-w-screen-xl">
+				<Form {...form}>
+					<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+						<FormField
+							control={form.control}
+							name="file"
+							render={() => (
+								<FormItem>
+									<FormLabel>Upload an image</FormLabel>
+									<FormControl>
+										<Input type="file" {...form.register("file")} />
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+						<Button type="submit">Submit</Button>
+					</form>
+				</Form>
+				<div className="grid w-full max-w-sm items-center gap-1.5"></div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
