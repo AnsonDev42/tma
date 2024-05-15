b2c4d00e03e4921e33cfd6a5cbbdf78533da0960
Anson <58594437+AnsonDev42@users.noreply.github.com>
2024-05-16 00:19:05 +0100

    add zoom-in-out component

--- frontend/src/components/dish.tsx
+++ frontend/src/components/dish.tsx
@@ -5,6 +5,7 @@ import {
 	PopoverTrigger,
 } from "@/components/ui/popover";
 import { useEffect, useState } from "react";
+import { TransformComponent, TransformWrapper } from "react-zoom-pan-pinch";
 
 interface ImageResultsProps {
 	menuSrc: string | ArrayBuffer | null;
@@ -80,65 +81,76 @@ export function ImageResults({
 	});
 
 	return (
-		<div className="relative">
-			<img
-				src={menuSrc as string}
-				alt="Uploaded"
-				ref={imageRef}
-				className="absolute max-w-lg"
-			/>
-			{data.map((value, index) => (
-				<Popover key={index}>
-					<PopoverTrigger asChild>
-						<div
-							key={index}
-							className="absolute"
-							style={getAdjustedStyles(value.boundingBox)}
-						>
-							{showText && (
-								<div style={getTextStyle(value.boundingBox)}>
-									{value.info.text}
-								</div>
-							)}
-						</div>
-					</PopoverTrigger>
-					<PopoverContent className="max-w-sm p-6 bg-white rounded-lg shadow-lg">
-						<div className="flex flex-col items-center">
-							{value.info.imgSrc && (
-								<img
-									src={value.info.imgSrc}
-									alt={`${value.info.text} image`}
-									className="w-full h-40 object-cover rounded-t-lg mb-4"
-								/>
-							)}
-							<h2 className="font-bold text-xl mb-2">{value.info.text}</h2>
-							<p className="text-gray-700 mb-4">{value.info.description}</p>
-							<div className="flex space-x-4">
-								<a
-									href={`https://www.google.com/search?q=${encodeURIComponent(
-										value.info.text,
-									)}`}
-									target="_blank"
-									rel="noopener noreferrer"
-									className="text-blue-500 hover:underline"
-								>
-									Search on Google
-								</a>
-								<a
-									href={`https://wikipedia.org/wiki/${encodeURIComponent(
-										value.info.text,
-									)}`}
-									target="_blank"
-									rel="noopener noreferrer"
-									className="text-blue-500 hover:underline"
-								>
-									View on Wikipedia
-								</a>
-							</div>
-						</div>
-					</PopoverContent>
-				</Popover>
-			))}
+		<div className="flex justify-center items-center p-3 m-2 bg-blue-500 border border-gray-300 rounded-2xl ">
+			<TransformWrapper>
+				<TransformComponent>
+					<div className=" relative  max-w-full max-h-screen flex items-start">
+						<img
+							src={menuSrc as string}
+							alt="Uploaded"
+							ref={imageRef}
+							className="max-w-full max-h-screen relative"
+						/>
+
+						{data.map((value, index) => (
+							<Popover key={index}>
+								<PopoverTrigger asChild>
+									<div
+										key={index}
+										className="absolute"
+										style={getAdjustedStyles(value.boundingBox)}
+									>
+										{showText && (
+											<div style={getTextStyle(value.boundingBox)}>
+												{value.info.text}
+											</div>
+										)}
+									</div>
+								</PopoverTrigger>
+								<PopoverContent className="max-w-sm p-6 bg-white rounded-lg shadow-lg">
+									<div className="flex flex-col items-center">
+										{value.info.imgSrc && (
+											<img
+												src={value.info.imgSrc}
+												alt={`${value.info.text} image`}
+												className="w-full h-40 object-cover rounded-t-lg mb-4"
+											/>
+										)}
+										<h2 className="font-bold text-xl mb-2">
+											{value.info.text}
+										</h2>
+										<p className="text-gray-700 mb-4">
+											{value.info.description}
+										</p>
+										<div className="flex space-x-4">
+											<a
+												href={`https://www.google.com/search?q=${encodeURIComponent(
+													value.info.text,
+												)}`}
+												target="_blank"
+												rel="noopener noreferrer"
+												className="text-blue-500 hover:underline"
+											>
+												Search on Google
+											</a>
+											<a
+												href={`https://wikipedia.org/wiki/${encodeURIComponent(
+													value.info.text,
+												)}`}
+												target="_blank"
+												rel="noopener noreferrer"
+												className="text-blue-500 hover:underline"
+											>
+												View on Wikipedia
+											</a>
+										</div>
+									</div>
+								</PopoverContent>
+							</Popover>
+						))}
+					</div>
+				</TransformComponent>
+			</TransformWrapper>
 		</div>
 	);
 }
