73aa7c6ab46b714c207903502f5501e44bdf6c21
Anson <58594437+AnsonDev42@users.noreply.github.com>
2024-05-19 15:45:27 +0100

    add daisyUI and theme switcher

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -355,50 +355,88 @@ function MainAppContent() {
 
 	return (
 		<div>
-			<LanguageComboBox />
-			<Authentication />
-			<h1 className="text-3xl font-bold text-center mt-5">Menu Analyzer</h1>
-			<div className="max-w-lg">
-				<Form {...form}>
-					<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-						<FormField
-							control={form.control}
-							name="file"
-							render={() => (
-								<FormItem>
-									<FormLabel>Upload a Menu image</FormLabel>
-									<FormControl>
-										<Input type="file" {...form.register("file")} />
-									</FormControl>
-									<FormMessage />
-								</FormItem>
-							)}
-						/>
-						<Button type="submit">Upload</Button>
-					</form>
-				</Form>
-				<div className="w-full"></div>
-			</div>
-			{/* Toggle for showing/hiding text */}
-			<div className="mt-4">
-				<div className="flex items-center space-x-2">
-					<Switch
-						id="Show Bounding Box Text"
-						checked={showText}
-						onCheckedChange={handleToggleText}
+			<html data-theme="">
+				<LanguageComboBox />
+				<Authentication />
+				<label className="flex cursor-pointer gap-2">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="20"
+						height="20"
+						viewBox="0 0 24 24"
+						fill="none"
+						stroke="currentColor"
+						strokeWidth="2"
+						strokeLinecap="round"
+						strokeLinejoin="round"
+					>
+						<circle cx="12" cy="12" r="5" />
+						<path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" />
+					</svg>
+					<input
+						type="checkbox"
+						value="halloween"
+						className="toggle theme-controller"
 					/>
-					<Label htmlFor="Show-Bounding-Box-Text">Show Bounding Box Text</Label>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="20"
+						height="20"
+						viewBox="0 0 24 24"
+						fill="none"
+						stroke="currentColor"
+						strokeWidth="2"
+						strokeLinecap="round"
+						strokeLinejoin="round"
+					>
+						<path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
+					</svg>
+				</label>
+				<h1 className="text-3xl font-bold text-center mt-5">Menu Analyzer</h1>
+				<div className="max-w-lg">
+					<Form {...form}>
+						<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+							<FormField
+								control={form.control}
+								name="file"
+								render={() => (
+									<FormItem>
+										<FormLabel>Upload a Menu image</FormLabel>
+										<FormControl>
+											<Input type="file" {...form.register("file")} />
+										</FormControl>
+										<FormMessage />
+									</FormItem>
+								)}
+							/>
+							<Button type="submit">Upload</Button>
+						</form>
+					</Form>
+					<div className="w-full"></div>
 				</div>
-			</div>
-			{menuSrc && (
-				<ImageResults
-					menuSrc={menuSrc}
-					data={data}
-					imageRef={imageRef}
-					showText={showText}
-					imageResultsRef={imageResultsRef}
-				/>
-			)}
+				{/* Toggle for showing/hiding text */}
+				<div className="mt-4">
+					<div className="flex items-center space-x-2">
+						<Switch
+							id="Show Bounding Box Text"
+							checked={showText}
+							onCheckedChange={handleToggleText}
+						/>
+						<Label htmlFor="Show-Bounding-Box-Text">
+							Show Bounding Box Text
+						</Label>
+					</div>
+				</div>
+				{menuSrc && (
+					<ImageResults
+						menuSrc={menuSrc}
+						data={data}
+						imageRef={imageRef}
+						showText={showText}
+						imageResultsRef={imageResultsRef}
+					/>
+				)}
+			</html>
 		</div>
 	);
 }
