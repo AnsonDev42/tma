9fc228c4911a088fb682fe28a880f4b5b107792d
Anson <58594437+AnsonDev42@users.noreply.github.com>
2024-05-19 18:21:20 +0100

    add navbar; rename files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { useForm } from "react-hook-form";
 import "./globals.css";
-import { ImageResults } from "@/components/dish.tsx";
+import { ImageResults } from "@/components/Dish.tsx";
+import { Navbar } from "@/components/Navbar.tsx";
 import {
 	Command,
 	CommandEmpty,
@@ -347,43 +348,9 @@ function MainAppContent() {
 	return (
 		<div>
 			<html data-theme="">
-				<LanguageComboBox />
+				<Navbar />
 				<Authentication />
-				<label className="flex cursor-pointer gap-2">
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						width="20"
-						height="20"
-						viewBox="0 0 24 24"
-						fill="none"
-						stroke="currentColor"
-						strokeWidth="2"
-						strokeLinecap="round"
-						strokeLinejoin="round"
-					>
-						<circle cx="12" cy="12" r="5" />
-						<path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" />
-					</svg>
-					<input
-						type="checkbox"
-						value="halloween"
-						className="toggle theme-controller"
-					/>
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						width="20"
-						height="20"
-						viewBox="0 0 24 24"
-						fill="none"
-						stroke="currentColor"
-						strokeWidth="2"
-						strokeLinecap="round"
-						strokeLinejoin="round"
-					>
-						<path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
-					</svg>
-				</label>
-				<h1 className="text-3xl font-bold text-center mt-5">Menu Analyzer</h1>
+
 				<div className="max-w-lg">
 					<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
 						<label className="form-control w-full max-w-xs">
