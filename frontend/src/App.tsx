9d08f9fa4156f9202e9ba19b4a85cd1732e0d2a0
Anson <58594437+AnsonDev42@users.noreply.github.com>
2024-05-19 19:19:38 +0100

    refactor Authentication

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,119 +3,13 @@ import { ImageResults } from "@/components/Dish.tsx";
 import { Navbar } from "@/components/Navbar.tsx";
 import UploadForm from "@/components/UploadForm.tsx";
 
+import { Authentication } from "@/components/Authentication.tsx";
 import { Label } from "@/components/ui/label";
 import { Switch } from "@/components/ui/switch";
 import { SessionProvider } from "@/contexts/SessionContext";
 import { DishProps } from "@/types/DishProps.tsx";
-import { Turnstile } from "@marsidev/react-turnstile";
-import { Auth } from "@supabase/auth-ui-react";
-import { ThemeSupa } from "@supabase/auth-ui-shared";
-import { Session } from "@supabase/gotrue-js/src/lib/types";
-import { createClient } from "@supabase/supabase-js";
 import { useEffect, useRef, useState } from "react";
-import { Toaster, toast } from "sonner";
-import { Button } from "./components/ui/button";
-
-const supabase = createClient(
-	"https://scwodhehztemzcpsofzy.supabase.co",
-	"REMOVED",
-);
-
-function Authentication() {
-	const [session, setSession] = useState<Session | null>(null);
-	const [captchaToken, setCaptchaToken] = useState<string>("");
-
-	useEffect(() => {
-		const {
-			data: { subscription },
-		} = supabase.auth.onAuthStateChange((_event, session) => {
-			setSession(session);
-		});
-
-		return () => subscription.unsubscribe();
-	}, []);
-	useEffect(() => {
-		supabase.auth.onAuthStateChange(async (event, _auth) => {
-			if (event == "PASSWORD_RECOVERY") {
-				const newPassword = prompt("Enter your a new password ");
-				const { data, error } = await supabase.auth.updateUser({
-					password: newPassword as string,
-				});
-
-				if (data) {
-					toast.info("Password updated successfully!");
-				}
-				if (error) {
-					alert("There was an error updating your password.");
-				}
-			}
-		});
-	}, []);
-	async function handleAnonymousSignIn() {
-		try {
-			const {
-				data: { session },
-				error,
-			} = await supabase.auth.signInAnonymously({ options: { captchaToken } });
-			if (error) {
-				throw error;
-			}
-			toast.success("Signed in anonymously!");
-			setSession(session);
-		} catch (_error) {
-			console.error("Error signing in anonymously.");
-			alert("Failed to sign in anonymously.");
-			toast.error("Failed to sign in anonymously.");
-		}
-	}
-	if (!session) {
-		return (
-			<div className="flex items-center justify-center max-w-full mt-5">
-				<div className="flex-col items-center justify-center">
-					<Auth
-						supabaseClient={supabase}
-						appearance={{
-							theme: ThemeSupa,
-							variables: {
-								default: {
-									colors: {
-										brand: "green",
-										brandAccent: "darkgreen",
-									},
-								},
-							},
-						}}
-						providers={["google", "github"]}
-					/>
-					<h1>Demo Sign in : </h1>
-					<Button onClick={handleAnonymousSignIn} className="m-3">
-						Sign in anonymously
-					</Button>
-					<Turnstile
-						siteKey="0x4AAAAAAAaDaYB6f6UNZHsB"
-						onSuccess={(token) => {
-							setCaptchaToken(token);
-						}}
-					/>
-				</div>
-			</div>
-		);
-	} else {
-		return (
-			<div className="flex-row">
-				<h1>Welcome, {session.user.email}</h1>
-				<Button
-					onClick={async () => {
-						await supabase.auth.signOut();
-						toast.success("Signed out successfully!");
-					}}
-				>
-					Sign out
-				</Button>
-			</div>
-		);
-	}
-}
+import { Toaster } from "sonner";
 
 function App() {
 	return (
