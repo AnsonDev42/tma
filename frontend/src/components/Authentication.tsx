6f1440793583837a82bf94203ef25785f3a2d207
Anson <58594437+AnsonDev42@users.noreply.github.com>
2024-05-19 19:19:38 +0100

    refactor Authentication

---
+++ frontend/src/components/Authentication.tsx
@@ -0,0 +1,111 @@
+import { Button } from "@/components/ui/button.tsx";
+import { Turnstile } from "@marsidev/react-turnstile";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+import { Session } from "@supabase/gotrue-js/src/lib/types.ts";
+import { createClient } from "@supabase/supabase-js";
+import { useEffect, useState } from "react";
+import { toast } from "sonner";
+
+const supabase = createClient(
+	"https://scwodhehztemzcpsofzy.supabase.co",
+	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNjd29kaGVoenRlbXpjcHNvZnp5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTUxMjA3MTksImV4cCI6MjAzMDY5NjcxOX0.j4O3aKoITFoJi36sQiPoh5PUWSDwwDDh02hhmMRF8HY",
+);
+
+export function Authentication() {
+	const [session, setSession] = useState<Session | null>(null);
+	const [captchaToken, setCaptchaToken] = useState<string>("");
+
+	useEffect(() => {
+		const {
+			data: { subscription },
+		} = supabase.auth.onAuthStateChange((_event, session) => {
+			setSession(session);
+		});
+
+		return () => subscription.unsubscribe();
+	}, []);
+	useEffect(() => {
+		supabase.auth.onAuthStateChange(async (event, _auth) => {
+			if (event == "PASSWORD_RECOVERY") {
+				const newPassword = prompt("Enter your a new password ");
+				const { data, error } = await supabase.auth.updateUser({
+					password: newPassword as string,
+				});
+
+				if (data) {
+					toast.info("Password updated successfully!");
+				}
+				if (error) {
+					alert("There was an error updating your password.");
+				}
+			}
+		});
+	}, []);
+	async function handleAnonymousSignIn() {
+		try {
+			const {
+				data: { session },
+				error,
+			} = await supabase.auth.signInAnonymously({ options: { captchaToken } });
+			if (error) {
+				throw error;
+			}
+			toast.success("Signed in anonymously!");
+			setSession(session);
+		} catch (_error) {
+			console.error("Error signing in anonymously.");
+			alert("Failed to sign in anonymously.");
+			toast.error("Failed to sign in anonymously.");
+		}
+	}
+	if (!session) {
+		return (
+			<div className="flex items-center justify-center max-w-full mt-5">
+				<div className="flex-col items-center justify-center">
+					<Auth
+						supabaseClient={supabase}
+						appearance={{
+							theme: ThemeSupa,
+							variables: {
+								default: {
+									colors: {
+										brand: "green",
+										brandAccent: "darkgreen",
+									},
+								},
+							},
+						}}
+						providers={["google", "github"]}
+					/>
+					<h1>Demo Sign in : </h1>
+					<Button onClick={handleAnonymousSignIn} className="m-3">
+						Sign in anonymously
+					</Button>
+					<Turnstile
+						siteKey="0x4AAAAAAAaDaYB6f6UNZHsB"
+						onSuccess={(token) => {
+							setCaptchaToken(token);
+						}}
+					/>
+				</div>
+			</div>
+		);
+	} else {
+		return (
+			<div className="flex-row">
+				<h1>
+					Welcome, {session.user.email == "" ? "Demo User" : session.user.email}
+				</h1>
+				<Button
+					onClick={async () => {
+						await supabase.auth.signOut();
+						toast.success("Signed out successfully!");
+					}}
+				>
+					Sign out
+				</Button>
+			</div>
+		);
+	}
+}
