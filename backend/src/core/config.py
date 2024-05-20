4c6899db00646a842a67a49d4685b9f8fb162a86
Han Hwang Lim <hanhwanglim@gmail.com>
2024-05-20 18:37:04 +0100

    Create core package

---
+++ backend/src/core/config.py
@@ -0,0 +1,28 @@
+import secrets
+from typing import Annotated, Any
+
+from pydantic import AnyUrl, BeforeValidator
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+def parse_cors(v: Any) -> list[str] | str:
+    if isinstance(v, str) and not v.startswith("["):
+        return [i.strip() for i in v.split(",")]
+    elif isinstance(v, list | str):
+        return v
+    raise ValueError(v)
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env", env_ignore_empty=True, extra="ignore"
+    )
+
+    SECRET_KEY: str = secrets.token_urlsafe(32)
+    OPENAI_API_KEY: str
+    BACKEND_CORS_ORIGINS: Annotated[
+        list[AnyUrl] | str, BeforeValidator(parse_cors)
+    ] = []
+
+
+settings = Settings()
---
+++ backend/src/core/security.py
@@ -0,0 +1,12 @@
+from jose import jwt
+
+from src.core.config import settings
+
+ALGORITHM = "HS256"
+
+
+def verify_jwt(token: str):
+    payload = jwt.decode(
+        token, settings.SECRET_KEY, algorithms=[ALGORITHM], audience="authenticated"
+    )
+    return payload
