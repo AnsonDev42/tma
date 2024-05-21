0985f1270e77a9286daa78be28b5350618012312
Han Hwang Lim <hanhwanglim@gmail.com>
2024-05-21 23:03:11 +0100

    Integrate ocr

--- backend/src/services/utils.py
+++ backend/src/services/utils.py
@@ -1,6 +1,7 @@
 import functools
 import logging
 import time
+from dataclasses import dataclass
 from typing import Callable, ParamSpec, TypeVar
 
 P = ParamSpec("P")
@@ -17,3 +18,26 @@ def duration(func: Callable[P, T]) -> Callable[P, T]:
         return result
 
     return wrapper
+
+
+@dataclass
+class BoundingBox:
+    """Bounding box for detected text in image in percentage"""
+
+    x: float
+    y: float
+    w: float
+    h: float
+
+
+PROMPT = """\
+You are a helpful assistant specialized in food industry and translation, designed to 
+output structured JSON response with keys 'dish-name' and 'dish-description', 
+in the user's preferred language code: Target-Language=%s. The 
+'dish-name' should be the cleaned-up version of the OCR result in $Target-Language; 
+'dish-name' may contains prices of the dish such as '- 4', please remove them.
+the 'dish-description' should be a brief introduction to the dish based on its name in $Target-Language.
+If you don't think the OCR result is relevant to food, it may due to OCR errors or the texts is 
+not a dish, like a restaurant name, or price info. In this case, please return value of 
+'dish-name' as its original OCR result and 'dish-description' as null.
+"""
