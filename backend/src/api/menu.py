620ce488e4b09858c8e5ef1e6128a0fa748d813a
Han Hwang Lim <hanhwanglim@gmail.com>
2024-05-21 23:03:11 +0100

    Integrate ocr

--- backend/src/api/menu.py
+++ backend/src/api/menu.py
@@ -1,11 +1,15 @@
 import http
+from io import BytesIO
 
+import cv2
 import ujson
 from fastapi import APIRouter, Depends, UploadFile, HTTPException
+from starlette.responses import StreamingResponse
 
 from src.api.deps import get_user
 from src.models import User
-from src.services.menu import run_ocr
+from src.services.menu import run_ocr, process_ocr_results, compress_image, process_image, normalize_text_bbox, \
+    serialize_dish_data, get_dish_info_via_openai
 from src.test.fixtures import FIXTURES
 
 router = APIRouter()
@@ -15,8 +19,13 @@ router = APIRouter()
 async def upload(file: UploadFile, user: User = Depends(get_user)):
     if not file or not file.filename:
         raise HTTPException(status_code=http.HTTPStatus.BAD_REQUEST, detail="No file uploaded")
-    img_height, img_width, ocr_results = run_ocr(file)
-    return {"results": ...}
+
+    image, img_height, img_width = process_image(file)
+    ocr_results = run_ocr(image)
+    dish_info = await process_ocr_results(ocr_results)
+    bounding_box = normalize_text_bbox(img_width, img_height, ocr_results)
+    data = serialize_dish_data(dish_info, bounding_box)
+    return {"results": data}
 
 
 @router.post("/test")
@@ -26,3 +35,31 @@ async def test_upload(file: UploadFile):
 
     with open(FIXTURES, "test1-bbox-results.json", encoding="utf-8") as f:
         return ujson.load(f)
+
+@router.get("/get_dish_info")
+async def dish_info_pipeline(dish: str):
+    """
+    for Swift App usage
+    return a dish info from wikipedia search
+    results contains description, image(url), and text
+    """
+    dish_info = await get_dish_info_via_openai(dish, "en")
+    return {"results": dish_info, "message": "OK"}
+
+
+@router.post("/draw")
+async def draw(file: UploadFile):
+    image, img_height, img_width = process_image(file)
+    ocr_results = run_ocr(image)
+    bounding_boxes = normalize_text_bbox(img_width, img_height, ocr_results)
+
+    for bbox in bounding_boxes:
+        x = int(bbox["x"] * img_width / 100)
+        y = int(bbox["y"] * img_height / 100)
+        w = int(bbox["w"] * img_width / 100)
+        h = int(bbox["h"] * img_height / 100)
+        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 3)
+
+    # Encode the image to be able to return it in HTTP response
+    _, buffered = cv2.imencode(".jpg", image)
+    return StreamingResponse(BytesIO(buffered), media_type="image/jpeg")
--- backend/src/services/menu.py
+++ backend/src/services/menu.py
@@ -1,23 +1,27 @@
 import ast
+import asyncio
 import base64
+from dataclasses import asdict
 from typing import Any
 
 import cv2
 import numpy as np
 import requests
 import ujson
+from httpx import AsyncClient
 
-from src.core.vendor import PD_OCR_API_URL
+from src.core.vendor import PD_OCR_API_URL, client, WIKI_API_URL
 from src.services.exceptions import OCRError
-from src.services.utils import duration
+from src.services.utils import duration, PROMPT, BoundingBox
 
 MAX_IMAGE_WIDTH = 550
 TIMEOUT = 60
 
 
 @duration
-def compress_image(img_nparr: np.ndarray) -> tuple[str, int, int]:
-    img = cv2.imdecode(img_nparr, cv2.IMREAD_COLOR)
+def process_image(image: bytes) -> tuple[str, int, int]:
+    data = np.frombuffer(image, np.uint8)
+    img = cv2.imdecode(data, cv2.IMREAD_COLOR)
     img_height, img_width, _ = img.shape
 
     if img_width > MAX_IMAGE_WIDTH:
@@ -36,15 +40,12 @@ def compress_image(img_nparr: np.ndarray) -> tuple[str, int, int]:
 
 
 @duration
-def run_ocr(file_content: bytes) -> tuple[int, int, Any]:
+def run_ocr(image: bytes) -> Any:
     """
     Post uploaded image file content to pdOCR server and return img dimension and OCR results in json format
     return: (img_height, img_width), ocr_results
     """
     # convert image from bytes and compress image
-    img_nparr = np.frombuffer(file_content, np.uint8)
-    image, img_height, img_width = compress_image(img_nparr)
-
     data = {"key": ["image"], "value": [image]}
     response = requests.post(url=PD_OCR_API_URL, data=ujson.dumps(data), timeout=TIMEOUT)
     response.raise_for_status()
@@ -58,4 +59,86 @@ def run_ocr(file_content: bytes) -> tuple[int, int, Any]:
     if not isinstance(ocr_results, list) or not ocr_results:
         raise OCRError("Failed to parse OCR results")
 
-    return img_width, img_height, ocr_results
+    return ocr_results
+
+
+async def get_dish_info_via_openai(dish_name: str, accept_language: str) -> dict:
+    """Search dish info via OPENAI and using WIKI to get the image"""
+    response = await client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        response_format={"type": "json_object"},
+        messages=[
+            {"role": "system", "content": PROMPT.format(accept_language)},
+            {"role": "user", "content": f"OCR result: '{dish_name}'"},
+        ],
+    )
+    content = ujson.loads(response.choices[0].message.content)
+    return {"description": content["dish-description"], "text": content["dish-name"]}
+
+
+async def get_dish_image(dish_name: str | None) -> str | None:
+    if dish_name is None:
+        return None
+
+    query = {"q": dish_name, "format": "json", "limit": "3"}
+    async with AsyncClient() as ac:
+        response = await ac.get(WIKI_API_URL, params=query)
+    response.raise_for_status()
+    result = response.json()
+
+    for item in result.get("pages", []):
+        thumbnail = item.get("thumbnail")
+        if thumbnail and "url" in thumbnail:
+            return f"https:{thumbnail['url']}"
+    return None
+
+
+def get_dish_data(dish_name: str, accept_language: str) -> dict:
+    data = get_dish_info_via_openai(dish_name, accept_language)
+    img_src = get_dish_image(data)
+    return data | {"img_src": img_src}
+
+
+async def process_ocr_results(ocr_results: list) -> list[dict]:
+    tasks = []
+    for item in ocr_results:
+        dish_name, _ = item[0]
+        tasks.append(get_dish_data(dish_name, "en"))
+    return await asyncio.gather(*tasks)
+
+
+def normalize_text_bbox(img_width, img_height, ocr_results: list):
+    texts_bboxes = []
+    for item in ocr_results:
+        bounding_boxes = item[1]
+        texts_bboxes.append(
+            asdict(normalize_bounding_box(img_width, img_height, bounding_boxes))
+        )
+    return texts_bboxes
+
+
+def normalize_bounding_box(
+        img_width, img_height, bounding_box: list
+) -> BoundingBox:
+    """
+    Calculate the bounding box of the detected text in image
+    """
+    x_min = min([x[0] for x in bounding_box])
+    x_max = max([x[0] for x in bounding_box])
+    y_min = min([x[1] for x in bounding_box])
+    y_max = max([x[1] for x in bounding_box])
+
+    x_percentage = x_min / img_width
+    y_percentage = y_min / img_height
+    w_percentage = (x_max - x_min) / img_width
+    h_percentage = (y_max - y_min) / img_height
+
+    return BoundingBox(x=x_percentage, y=y_percentage, w=w_percentage, h=h_percentage)
+
+
+def serialize_dish_data(dish_data: list, bounding_boxes: list):
+    results = []
+    for dish_info, dish_bbox in zip(dish_data, bounding_boxes, strict=True):
+        dish = { "info": dish_info, "boundingBox": dish_bbox }
+        results.append(dish)
+    return results
