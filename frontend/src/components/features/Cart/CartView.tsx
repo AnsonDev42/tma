02af420369412e94ab2fe332d8497f754cbbf6b4
Anson <58594437+AnsonDev42@users.noreply.github.com>
2024-05-28 01:20:11 +0100

    refactor Cart hook

--- frontend/src/components/features/Cart/CartView.tsx
+++ frontend/src/components/features/Cart/CartView.tsx
@@ -1,58 +1,19 @@
-import { Cart, CartItem } from "@/types/CartTypes";
-import { DishProps } from "@/types/DishProps";
-import {
-	deleteCart,
-	getCartByName,
-	getDishInfoByIdAndTimestamp,
-	removeDishFromCart,
-} from "@/utils/localStorageCartUtils";
-import React, { useEffect, useState } from "react";
+import { useCart } from "@/utils/hooks/useCart.ts";
+import React from "react";
 
 const CartView: React.FC = () => {
-	const [cart, setCart] = useState<Cart | null>(getCartByName("My Cart"));
-
-	useEffect(() => {
-		const handleStorageEvent = () => {
-			setCart(getCartByName("My Cart"));
-		};
-
-		window.addEventListener("storage", handleStorageEvent);
-
-		return () => {
-			window.removeEventListener("storage", handleStorageEvent);
-		};
-	}, []);
+	const { cart, dishes, deleteCurrentCart, removeDish } = useCart("My Cart");
 
 	if (!cart) {
 		return <div>No cart found</div>;
 	}
-
-	const dishes = cart.items.map((item: CartItem) => {
-		return {
-			dish: getDishInfoByIdAndTimestamp(
-				item.dishId,
-				item.uploadTimestamp,
-			) as DishProps,
-			timestamp: item.uploadTimestamp,
-		};
-	});
-
-	const handleDeleteCart = () => {
-		deleteCart(cart.name);
-		setCart(null);
-	};
-
-	const handleRemoveDish = (dishId: number, timestamp: string) => {
-		removeDishFromCart(cart.name, dishId, timestamp);
-		setCart(getCartByName("My Cart"));
-	};
 
 	return (
 		<div>
 			<div className="items-start justify-between m-0.5 grid grid-flow-col text-wrap break-words">
 				{cart.name} ({dishes.length})
 				<button
-					onClick={handleDeleteCart}
+					onClick={deleteCurrentCart}
 					className="btn btn-warning btn-xs l-2"
 				>
 					Delete All
@@ -73,7 +34,7 @@ const CartView: React.FC = () => {
 							</p>
 						</div>
 						<button
-							onClick={() => handleRemoveDish(dish.dish.id, dish.timestamp)}
+							onClick={() => removeDish(dish.dish.id, dish.timestamp)}
 							className="text-red-500 underline ml-2"
 						>
 							Delete
