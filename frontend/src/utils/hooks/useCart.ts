ece37570e1e6b2d730d6bcbc6a0ec893fe967290
Anson <58594437+AnsonDev42@users.noreply.github.com>
2024-05-28 01:20:11 +0100

    refactor Cart hook

---
+++ frontend/src/utils/hooks/useCart.ts
@@ -0,0 +1,46 @@
+import { Cart, CartItem } from "@/types/CartTypes.ts";
+import { DishProps } from "@/types/DishProps.tsx";
+import {
+	deleteCart,
+	getCartByName,
+	getDishInfoByIdAndTimestamp,
+	removeDishFromCart,
+} from "@/utils/localStorageCartUtils.ts";
+import { useEffect, useState } from "react";
+
+export const useCart = (cartName: string) => {
+	const [cart, setCart] = useState<Cart | null>(getCartByName(cartName));
+
+	useEffect(() => {
+		const handleStorageEvent = () => {
+			setCart(getCartByName(cartName));
+		};
+
+		window.addEventListener("storage", handleStorageEvent);
+
+		return () => {
+			window.removeEventListener("storage", handleStorageEvent);
+		};
+	}, [cartName]);
+
+	const deleteCurrentCart = () => {
+		deleteCart(cartName);
+		setCart(null);
+	};
+
+	const removeDish = (dishId: number, timestamp: string) => {
+		removeDishFromCart(cartName, dishId, timestamp);
+		setCart(getCartByName(cartName));
+	};
+
+	const dishes =
+		cart?.items.map((item: CartItem) => ({
+			dish: getDishInfoByIdAndTimestamp(
+				item.dishId,
+				item.uploadTimestamp,
+			) as DishProps,
+			timestamp: item.uploadTimestamp,
+		})) || [];
+
+	return { cart, dishes, deleteCurrentCart, removeDish };
+};
