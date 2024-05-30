003f5d0b0de3668888a81a5c5251f4f98eba9d71
Anson <58594437+AnsonDev42@users.noreply.github.com>
2024-05-30 18:10:02 +0100

    use react-router to navigate to /login and /home

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,135 +1,33 @@
 import "./globals.css";
 import { Authentication } from "@/components/features/Authentication/Authentication.tsx";
-import ImageCarousel from "@/components/features/Menu/ImageCarousel.tsx";
-import { ShowTextState } from "@/components/features/Menu/Menu.tsx";
-import UploadForm from "@/components/features/Menu/UploadForm.tsx";
-import Footer from "@/components/ui/Footer.tsx";
-import { Navbar } from "@/components/ui/Navbar.tsx";
-import Sidebar from "@/components/ui/Sidebar.tsx";
-import { SessionContext, SessionProvider } from "@/contexts/SessionContext";
-import { DishProps } from "@/types/DishProps.tsx";
-import { UploadProps } from "@/types/UploadProps.ts";
-import { useUploadsState } from "@/utils/hooks/useUploadsState.ts";
-import { useContext, useEffect, useRef, useState } from "react";
+import { SessionProvider } from "@/contexts/SessionContext";
+import { HomePage } from "@/pages/HomePage.tsx";
+import { ProtectedRoute } from "@/routers/router.tsx";
+import {
+	Navigate,
+	Route,
+	BrowserRouter as Router,
+	Routes,
+} from "react-router-dom";
 import { Toaster } from "sonner";
 
 function App() {
-	const session = useContext(SessionContext)?.session;
 	return (
 		<div>
 			<Toaster position="top-center" richColors />
 			<SessionProvider>
-				{session ? <MainAppContent /> : <Authentication />}
+				<div data-theme="">
+					<Router>
+						<Routes>
+							<Route element={<ProtectedRoute />}>
+								<Route path="/home" element={<HomePage />} />
+							</Route>
+							<Route path="/login" element={<Authentication />} />
+							<Route path="*" element={<Navigate to="/home" />} />
+						</Routes>
+					</Router>
+				</div>
 			</SessionProvider>
-		</div>
-	);
-}
-
-function MainAppContent() {
-	const [showTextState, setShowTextState] = useState(
-		ShowTextState.SHOW_ONLY_TRANSLATION,
-	);
-	const [menuSrc, setMenuSrc] = useState<string | ArrayBuffer | null>(null);
-	const [data, setData] = useState([] as DishProps[]);
-	const [imgTimestamp, setImgTimestamp] = useState<string | null>(null);
-	const imageResultsRef = useRef<HTMLDivElement | null>(null);
-	const toggleRef = useRef<HTMLInputElement | null>(null);
-	const { uploads } = useUploadsState();
-
-	useEffect(() => {
-		if (imageResultsRef.current) {
-			imageResultsRef.current.scrollIntoView({ behavior: "smooth" });
-		}
-	}, [showTextState, data, menuSrc, imgTimestamp]);
-
-	useEffect(() => {
-		if (toggleRef.current) {
-			if (showTextState == ShowTextState.SHOW_ONLY_TRANSLATION) {
-				toggleRef.current.indeterminate = true;
-				toggleRef.current.checked = false;
-			} else {
-				toggleRef.current.indeterminate = false;
-				toggleRef.current.checked = showTextState === ShowTextState.SHOW_BOTH;
-			}
-		}
-	}, [showTextState]);
-
-	const handleToggleTextState = () => {
-		setShowTextState((prev) => (prev + 1) % 3);
-	};
-
-	const handleSelectUpload = (upload: UploadProps) => {
-		// setMenuSrc(upload.imageSrc);
-		setData(upload.data);
-		setMenuSrc(upload.imageSrc);
-		setImgTimestamp(upload.timestamp);
-	};
-
-	return (
-		<div data-theme="">
-			<div className="drawer lg:drawer-open">
-				<input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
-				<div className="drawer-content flex flex-col">
-					<div className="fixed top-0 left-0 right-0 z-50">
-						<Navbar />
-					</div>
-					{/*	content */}
-					<div className="flex flex-col mx-2">
-						{/* authentication */}
-						<div className="divider divider-neutral"></div>
-						<Authentication />
-						<div className="divider divider-neutral"></div>
-
-						{/* upload form */}
-						<div className="max-w-lg">
-							<UploadForm onSelectUpload={handleSelectUpload} />
-							<div className="w-full"></div>
-						</div>
-						<div
-							className="divider divider-neutral"
-							ref={imageResultsRef}
-						></div>
-						{/* bounding box toggle*/}
-						<div className="mt-4">
-							<div> {imgTimestamp}</div>
-
-							<div className="flex items-center space-x-2">
-								<input
-									type="checkbox"
-									id="Show-Bounding-Box-Text"
-									className="toggle"
-									ref={toggleRef}
-									onChange={handleToggleTextState}
-								/>
-								<label htmlFor="Show-Bounding-Box-Text">
-									{showTextState === ShowTextState.HIDE_ALL
-										? "Hide all"
-										: showTextState === ShowTextState.SHOW_ONLY_TRANSLATION
-											? "Show Only Translation"
-											: "Show Both Text and Translation"}
-								</label>
-							</div>
-						</div>
-						{/* image results */}
-						<ImageCarousel
-							showTextState={showTextState}
-							uploads={uploads}
-							handleSelectUpload={handleSelectUpload}
-						/>
-					</div>
-				</div>
-				<div className="drawer-side">
-					<label
-						htmlFor="my-drawer-3"
-						aria-label="close sidebar"
-						className="drawer-overlay"
-					></label>
-					<Sidebar onSelectUpload={handleSelectUpload} />
-				</div>
-			</div>
-			<div className="mt-4">
-				<Footer />
-			</div>
 		</div>
 	);
 }
