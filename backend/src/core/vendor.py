1042e61ac110527238b7108363b796aa4932ea1a
Anson <58594437+AnsonDev42@users.noreply.github.com>
2024-05-30 13:43:24 +0100

    async supabase requests and setup singleton (a)client during the app startup

--- backend/src/core/vendor.py
+++ backend/src/core/vendor.py
@@ -1,7 +1,7 @@
 import logging
 import socket
 
-from supabase import create_client, Client
+from supabase._async.client import AsyncClient, create_client
 
 from src.core.config import settings
 from src.services.utils import build_search_chain
@@ -10,7 +10,20 @@ logger = logging.getLogger(__name__)
 
 chain = build_search_chain(model="gpt-4o")
 
-supabase: Client = create_client(settings.SUPABASE_URL, settings.SUPABASE_KEY)
+supabase: AsyncClient = None  # This will be initialized during startup
+
+
+async def initialize_supabase():
+    """this would be called during startup to initialize the supabase client, in main.py"""
+    global supabase
+    supabase = await create_client(settings.SUPABASE_URL, settings.SUPABASE_KEY)
+
+
+async def get_supabase_client() -> AsyncClient:
+    if supabase is None:
+        raise ValueError("Supabase client is not initialized")
+    return supabase
+
 
 try:
     IP = socket.gethostbyname("anson-eq.local")
