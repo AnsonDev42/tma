ccd0d07fa610327113af9179b4849e697aa0f6a4
Anson <58594437+AnsonDev42@users.noreply.github.com>
2024-08-03 20:47:07 +0100

    add pro use check for ai suggestion

--- backend/src/api/deps.py
+++ backend/src/api/deps.py
@@ -1,9 +1,9 @@
 import http
 
-from fastapi import Header, HTTPException
+from fastapi import Header, HTTPException, Depends
 
 from src.core.security import verify_jwt
-from src.models import User
+from src.models import User, Role
 
 
 def get_user(authorization: str = Header("Authorization")) -> User:
@@ -13,3 +13,13 @@ def get_user(authorization: str = Header("Authorization")) -> User:
         return User(**payload)
     except Exception:
         raise HTTPException(status_code=http.HTTPStatus.UNAUTHORIZED)
+
+
+def check_user_role(user: User, required_roles: list[Role]):
+    if user.user_role not in required_roles:
+        raise HTTPException(status_code=http.HTTPStatus.FORBIDDEN, detail="Insufficient privileges")
+    return user
+
+
+def get_pro_user(user: User = Depends(get_user)):
+    return check_user_role(user, [Role.PRO, Role.TRIAL, Role.FREE])
