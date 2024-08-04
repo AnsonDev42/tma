ebc5cee8dbca1158fce6c19c7bc731f0d2ed284a
Anson <58594437+AnsonDev42@users.noreply.github.com>
2024-08-04 01:46:40 +0100

    add ai-ask access limit (by time per day)

--- backend/src/api/__init__.py
+++ backend/src/api/__init__.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
 
-from src.api import root, menu
+from src.api import root, menu, user
 
 api_router = APIRouter()
 
 api_router.include_router(root.router)
 api_router.include_router(menu.router)
+api_router.include_router(user.router)
