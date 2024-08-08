d4965d592022a75c92006b918037684f282c3ef2
Anson <58594437+AnsonDev42@users.noreply.github.com>
2024-08-09 00:09:22 +0100

    move signout to avatar

--- frontend/src/components/features/Authentication/UserWelcomeBanner.tsx
+++ frontend/src/components/features/Authentication/UserWelcomeBanner.tsx
@@ -1,18 +1,12 @@
 import { LanguageComboBox } from "@/components/ui/LanguageComboBox";
 import { Session } from "@supabase/gotrue-js/src/lib/types";
-import { toast } from "sonner";
 import { UserInfoDropdown } from "./UserInfoDropdown";
 
 interface UserWelcomeBannerProps {
 	session: Session;
-	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
-	supabase: any;
 }
 
-export function UserWelcomeBanner({
-	session,
-	supabase,
-}: UserWelcomeBannerProps) {
+export function UserWelcomeBanner({ session }: UserWelcomeBannerProps) {
 	return (
 		<>
 			<div className="flex justify-between items-center gap-2 m-3">
@@ -22,17 +16,6 @@ export function UserWelcomeBanner({
 						{session.user.email === "" ? "Demo User " : session.user.email}
 					</h1>
 					<UserInfoDropdown />
-					<div>
-						<button
-							className="btn btn-link"
-							onClick={async () => {
-								await supabase.auth.signOut();
-								toast.success("Signed out successfully!");
-							}}
-						>
-							Sign out
-						</button>
-					</div>
 				</div>
 			</div>
 			<div className="max-w-xs ml-5 m-3">
