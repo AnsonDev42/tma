2ae9bee1e540ea3f715e5b201e073b4a24536bef
Anson <58594437+AnsonDev42@users.noreply.github.com>
2024-08-09 00:44:47 +0100

    remove username in welcome banner

--- frontend/src/components/features/Authentication/UserWelcomeBanner.tsx
+++ frontend/src/components/features/Authentication/UserWelcomeBanner.tsx
@@ -1,20 +1,13 @@
 import { LanguageComboBox } from "@/components/ui/LanguageComboBox";
-import { Session } from "@supabase/gotrue-js/src/lib/types";
 import { UserInfoDropdown } from "./UserInfoDropdown";
 
-interface UserWelcomeBannerProps {
-	session: Session;
-}
-
-export function UserWelcomeBanner({ session }: UserWelcomeBannerProps) {
+export function UserWelcomeBanner() {
 	return (
 		<>
 			<div className="flex justify-between items-center gap-2 m-3">
 				<div className="flex items-center">
 					<h1 className="italic">Welcome, </h1>
-					<h1 className="bold ml-1">
-						{session.user.email === "" ? "Demo User " : session.user.email}
-					</h1>
+					<h1 className="bold ml-1">please enjoy your time on The Menu App!</h1>
 					<UserInfoDropdown />
 				</div>
 			</div>
