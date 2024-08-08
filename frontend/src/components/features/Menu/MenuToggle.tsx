94c00af3418a2458a172dfb107942baecc1e47c9
Anson <58594437+AnsonDev42@users.noreply.github.com>
2024-08-08 17:19:59 +0100

    refactor menu toggle

---
+++ frontend/src/components/features/Menu/MenuToggle.tsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useRef } from 'react';
+import { ShowTextState } from "@/components/features/Menu/Menu.tsx";
+
+interface MenuToggleProps {
+    showTextState: number;
+    onToggle: () => void;
+}
+
+export const MenuToggle: React.FC<MenuToggleProps> = ({ showTextState, onToggle }) => {
+    const checkboxRef = useRef<HTMLInputElement>(null);
+    useEffect(() => {
+        if (checkboxRef.current) {
+            if (showTextState == ShowTextState.SHOW_ONLY_TRANSLATION) {
+                checkboxRef.current.indeterminate = true;
+                checkboxRef.current.checked = false;
+            } else {
+                checkboxRef.current.indeterminate = false;
+                checkboxRef.current.checked = showTextState === ShowTextState.SHOW_BOTH;
+            }
+        }
+    }, [showTextState]);
+    useEffect(() => {
+        if (checkboxRef.current) {
+            checkboxRef.current.indeterminate = showTextState === ShowTextState.SHOW_ONLY_TRANSLATION;
+        }
+    }, [showTextState]);
+    return (
+        <div className="flex items-center space-x-2">
+            <input
+                type="checkbox"
+                id="Show-Bounding-Box-Text"
+                className="toggle"
+                ref={checkboxRef}
+                onChange={onToggle}
+            />
+            <label htmlFor="Show-Bounding-Box-Text">
+                {showTextState === ShowTextState.HIDE_ALL
+                    ? "Hide all"
+                    : showTextState === ShowTextState.SHOW_ONLY_TRANSLATION
+                        ? "Show Only Translation"
+                        : "Show Both Text and Translation"}
+            </label>
+        </div>
+    );
+};
