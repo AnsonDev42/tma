92ad15f4b15b1af77f1eed382b125ad934f5107f
Anson <58594437+AnsonDev42@users.noreply.github.com>
2024-08-09 00:19:01 +0100

    new login UI

--- frontend/src/components/features/Authentication/LoginForm.tsx
+++ frontend/src/components/features/Authentication/LoginForm.tsx
@@ -1,65 +1,85 @@
-import { useState } from "react";
-import { Auth } from "@supabase/auth-ui-react";
-import { ThemeSupa } from "@supabase/auth-ui-shared";
 import { Button } from "@/components/ui/button";
 import { Turnstile } from "@marsidev/react-turnstile";
-import { toast } from "sonner";
-import { useNavigate } from "react-router-dom";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
 import { Session } from "@supabase/gotrue-js/src/lib/types";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "sonner";
 
 interface LoginFormProps {
-    supabase: any;
-    setSession: (session: Session | null) => void;
+	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+	supabase: any;
+	setSession: (session: Session | null) => void;
 }
 
 export function LoginForm({ supabase, setSession }: LoginFormProps) {
-    const [captchaToken, setCaptchaToken] = useState<string>("");
-    const navigate = useNavigate();
+	const [captchaToken, setCaptchaToken] = useState<string>("");
+	const navigate = useNavigate();
 
-    async function handleAnonymousSignIn() {
-        try {
-            const { data: { session }, error } = await supabase.auth.signInAnonymously({ options: { captchaToken } });
-            if (error) throw error;
-            toast.success("Signed in anonymously!");
-            setSession(session);
-            navigate("/home", { replace: true });
-        } catch (_error) {
-            console.error("Error signing in anonymously.");
-            toast.error("Failed to sign in anonymously.");
-        }
-    }
+	async function handleAnonymousSignIn() {
+		try {
+			const {
+				data: { session },
+				error,
+			} = await supabase.auth.signInAnonymously({ options: { captchaToken } });
+			if (error) throw error;
+			toast.success("Signed in anonymously!");
+			setSession(session);
+			navigate("/home", { replace: true });
+		} catch (_error) {
+			console.error("Error signing in anonymously.");
+			toast.error("Failed to sign in anonymously.");
+		}
+	}
 
-    return (
-        <div className="flex items-center justify-center max-w-full mt-5">
-            <div className="flex-col items-center justify-center">
-                <Auth
-                    supabaseClient={supabase}
-                    appearance={{
-                        theme: ThemeSupa,
-                        variables: {
-                            default: {
-                                colors: {
-                                    brand: "green",
-                                    brandAccent: "darkgreen",
-                                },
-                            },
-                        },
-                    }}
-                    providers={["google", "github"]}
-                />
-                <h1>
-                    <b>***Demo Sign in*** : </b>
-                </h1>
-                <Button onClick={handleAnonymousSignIn} className="m-3">
-                    Sign in anonymously
-                </Button>
-                <Turnstile
-                    siteKey="0x4AAAAAAAaDaYB6f6UNZHsB"
-                    onSuccess={(token) => {
-                        setCaptchaToken(token);
-                    }}
-                />
-            </div>
-        </div>
-    );
+	return (
+		<div className="hero bg-base-200 min-h-screen">
+			<div className="hero-content flex-col lg:flex-row-reverse">
+				<div className="text-center lg:text-left">
+					<h1 className="text-5xl font-bold">Login now!</h1>
+					<p className="py-6">
+						You are one step away from accessing <b>The Menu App</b>. Sign in
+						now to get started 🚀!
+					</p>
+					<p>
+						You don't need to connect or register by clicking below{" "}
+						<b>sign in anonymously</b> for now, or use the Google or GitHub
+						sign-in options.
+					</p>
+				</div>
+				<div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+					<div className="card-body">
+						<Auth
+							supabaseClient={supabase}
+							appearance={{
+								theme: ThemeSupa,
+								variables: {
+									default: {
+										colors: {
+											brand: "green",
+											brandAccent: "darkgreen",
+										},
+									},
+								},
+							}}
+							providers={["google", "github"]}
+						/>
+						<h1>
+							<b>***Demo Sign in*** : </b>
+						</h1>
+						<Button onClick={handleAnonymousSignIn} className="m-3">
+							Sign in anonymously
+						</Button>
+						<Turnstile
+							siteKey="0x4AAAAAAAaDaYB6f6UNZHsB"
+							onSuccess={(token) => {
+								setCaptchaToken(token);
+							}}
+						/>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
