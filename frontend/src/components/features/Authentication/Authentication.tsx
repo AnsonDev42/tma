9360b4e7c9fcf776ed4bec2841c54db8e1c3aa73
Anson <58594437+AnsonDev42@users.noreply.github.com>
2024-08-09 18:33:47 +0100

    update icon and login page

--- frontend/src/components/features/Authentication/Authentication.tsx
+++ frontend/src/components/features/Authentication/Authentication.tsx
@@ -1,14 +1,14 @@
-import supabase from "@/lib/supabaseClient";
-import { Session } from "@supabase/gotrue-js/src/lib/types.ts";
-import { useEffect, useState } from "react";
+import supabase from "@/lib/supabaseClient.ts";
+import { Session } from "inspector";
+import { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "sonner";
-import { LoginForm } from "./LoginForm.tsx";
-import { UserWelcomeBanner } from "./UserWelcomeBanner.tsx";
-
+import { LoginForm } from "./LoginForm";
+import { UserWelcomeBanner } from "./UserWelcomeBanner";
 export function Authentication() {
 	const [session, setSession] = useState<Session | null>(null);
 	const navigate = useNavigate();
+	const loginFormRef = useRef<HTMLDivElement>(null);
 	useEffect(() => {
 		const {
 			data: { subscription },
@@ -23,6 +23,7 @@ export function Authentication() {
 
 		return () => subscription.unsubscribe();
 	}, []);
+
 	useEffect(() => {
 		// workaround for build error
 		// biome-ignore lint/suspicious/noExplicitAny: <explanation>
@@ -43,9 +44,34 @@ export function Authentication() {
 		});
 	}, []);
 
-	if (!session) {
-		return <LoginForm supabase={supabase} setSession={setSession} />;
-	} else {
-		return <UserWelcomeBanner />;
-	}
+	const handleGetStarted = () => {
+		loginFormRef.current?.scrollIntoView({ behavior: "smooth" });
+	};
+
+	return (
+		<div>
+			<div className="hero bg-base-200 min-h-screen">
+				<div className="hero-content flex-col lg:flex-row-reverse">
+					<img
+						src="public/tma-icon.jpeg"
+						className="max-w-sm rounded-lg shadow-2xl"
+						alt="Hero image"
+					/>
+					<div>
+						<h1 className="text-5xl font-bold">Welcome to The Menu App!</h1>
+						<p className="py-6">
+							Discover and explore menus from various restaurants. Get
+							AI-powered recommendations and manage your favorite dishes.
+						</p>
+						<button className="btn btn-primary" onClick={handleGetStarted}>
+							Get Started
+						</button>
+					</div>
+				</div>
+			</div>
+			<div ref={loginFormRef}>
+				{!session ? <LoginForm /> : <UserWelcomeBanner />}
+			</div>
+		</div>
+	);
 }
