6e7b86c9c4483cc019e35f72f4704454aef4a3cc
Anson <58594437+AnsonDev42@users.noreply.github.com>
2024-08-25 17:48:24 +0100

    add font-size lib and reformat code

--- frontend/src/lib/supabaseClient.ts
+++ frontend/src/lib/supabaseClient.ts
@@ -2,7 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
 	"https://scwodhehztemzcpsofzy.supabase.co",
-	"REMOVED"
+	"REMOVED",
 );
 
 export default supabase;
