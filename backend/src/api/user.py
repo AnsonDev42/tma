d9233f46cfbe663ad44642d5f1f0077b8cea1b85
Yaowen.Shen <yaowen.shen@datalex.com>
2025-01-04 19:57:32 +0000

    add keep-alive endpoints to prevent supabase project shutdown

--- backend/src/api/user.py
+++ backend/src/api/user.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends
 
+from src.services.menu import retrieve_dish_image
 from src.api.deps import get_user
 from src.models import User
 from src.services.user import get_access_limits
@@ -13,3 +14,16 @@ async def get_user_info(user: User = Depends(get_user)):
     return the user role, usage and limits
     """
     return await get_access_limits(user)
+
+
+
+@router.get("/keep-alive")
+async def keep_alive():
+    """
+    Keep db live by query supabase
+    """
+    try:
+        await retrieve_dish_image("test_dish") # query supabase a random dish to keep db alive
+        return {"status": "success"}
+    except Exception as e:
+        return {"status": "failed", "error": str(e)}
