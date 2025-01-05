1d3a660fa056cfa86232d475aa58e34cc65d9db8
Yaowen.Shen <yaowen.shen@datalex.com>
2025-01-05 22:10:32 +0000

    add contact in footer

--- frontend/src/components/ui/Footer.tsx
+++ frontend/src/components/ui/Footer.tsx
@@ -2,7 +2,10 @@ function Footer() {
 	return (
 		<footer className="footer footer-center p-4 bg-base-300 text-base-content">
 			<aside>
-				<p>Copyright © 2025 - All right reserved by TMA. LLC.</p>
+				<p>Copyright © 2025 - All right reserved. </p>
+				<p>
+					<a href="mailto:tma@itsya0wen.com">Contact Us</a>
+				</p>
 			</aside>
 		</footer>
 	);
