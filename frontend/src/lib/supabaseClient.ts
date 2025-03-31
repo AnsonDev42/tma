219a6265a5401fc3c426c8e21605605f7ea60451
YW <58594437+ansondev42@users.noreply.github.com>
2025-03-31 23:55:42 +0100

    add VITE_ for configs

--- frontend/src/lib/supabaseClient.ts
+++ frontend/src/lib/supabaseClient.ts
@@ -1,12 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl =
-	import.meta.env.VITE_SUPABASE_URL ||
-	"https://scwodhehztemzcpsofzy.supabase.co";
-const supabaseAnonKey =
-	import.meta.env.VITE_SUPABASE_ANON_KEY ||
-	"REMOVED";
-
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || "";
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || "";
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 export default supabase;
