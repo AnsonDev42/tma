8e293c9f15bb64056c49f8ddba4534e8101d56ab
YW <58594437+AnsonDev42@users.noreply.github.com>
2025-03-31 23:53:58 +0100

    set supabase urls configurable

--- frontend/src/lib/supabaseClient.ts
+++ frontend/src/lib/supabaseClient.ts
@@ -1,8 +1,12 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabase = createClient(
-	"https://scwodhehztemzcpsofzy.supabase.co",
-	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNjd29kaGVoenRlbXpjcHNvZnp5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTUxMjA3MTksImV4cCI6MjAzMDY5NjcxOX0.j4O3aKoITFoJi36sQiPoh5PUWSDwwDDh02hhmMRF8HY",
-);
+const supabaseUrl =
+	import.meta.env.VITE_SUPABASE_URL ||
+	"https://scwodhehztemzcpsofzy.supabase.co";
+const supabaseAnonKey =
+	import.meta.env.VITE_SUPABASE_ANON_KEY ||
+	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNjd29kaGVoenRlbXpjcHNvZnp5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTUxMjA3MTksImV4cCI6MjAzMDY5NjcxOX0.j4O3aKoITFoJi36sQiPoh5PUWSDwwDDh02hhmMRF8HY";
+
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 export default supabase;
