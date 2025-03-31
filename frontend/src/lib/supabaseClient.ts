e67095493d2dc75f741cb55148bbd4e53904f138
YW <58594437+AnsonDev42@users.noreply.github.com>
2025-03-31 23:53:58 +0100

    set supabase urls configurable

--- frontend/src/lib/supabaseClient.ts
+++ frontend/src/lib/supabaseClient.ts
@@ -1,8 +1,12 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabase = createClient(
-	"https://scwodhehztemzcpsofzy.supabase.co",
-	"REMOVED",
-);
+const supabaseUrl =
+	import.meta.env.VITE_SUPABASE_URL ||
+	"https://scwodhehztemzcpsofzy.supabase.co";
+const supabaseAnonKey =
+	import.meta.env.VITE_SUPABASE_ANON_KEY ||
+	"REMOVED";
+
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 export default supabase;
