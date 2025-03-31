20fe0d45de7bcb07028f12f44bb0836be7c2953d
Yaowen.Shen <yaowen.shen@datalex.com>
2025-03-31 16:54:03 +0100

    remove repeated toaster

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,6 @@ import { Language, LanguageProvider } from "@/contexts/LanguageContext.tsx";
 import { SessionProvider } from "@/contexts/SessionContext.tsx";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Toaster } from "sonner";
 import App from "./App.tsx";
 
 const initialLanguage: Language = { value: "en-us", label: "English" };
@@ -11,7 +10,6 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
 		<SessionProvider>
 			<LanguageProvider initialLanguage={initialLanguage}>
-				<Toaster position="top-right" richColors />
 				<App />
 			</LanguageProvider>
 		</SessionProvider>
