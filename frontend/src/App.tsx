2d42dbd120df542996976f3e456bfb78ad6acc27
Yaowen.Shen <yaowen.shen@datalex.com>
2025-03-31 16:54:03 +0100

    remove repeated toaster

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,6 @@
 import "./globals.css";
 import { Authentication } from "@/components/features/Authentication/Authentication.tsx";
 import Footer from "@/components/ui/Footer.tsx";
-import { SessionProvider } from "@/contexts/SessionContext";
 import { UserInfoProvider } from "@/contexts/UserInfoContext.tsx";
 import { HomePage } from "@/pages/HomePage.tsx";
 import HomePageV2 from "@/pages/HomePageV2.tsx";
@@ -18,25 +17,23 @@ function App() {
 	return (
 		<div>
 			<Toaster position="top-center" richColors />
-			<SessionProvider>
-				<UserInfoProvider>
-					<div data-theme="">
-						<Router>
-							<Routes>
-								<Route element={<ProtectedRoute />}>
-									<Route path="/home" element={<HomePageV2 />} />
-									<Route path="/home/v1" element={<HomePage />} />
-									<Route path="*" element={<Navigate to="/home" />} />
-								</Route>
-								<Route path="/login" element={<Authentication />} />
-							</Routes>
-						</Router>
-						<div className="mt-4">
-							<Footer />
-						</div>
+			<UserInfoProvider>
+				<div data-theme="">
+					<Router>
+						<Routes>
+							<Route element={<ProtectedRoute />}>
+								<Route path="/home" element={<HomePageV2 />} />
+								<Route path="/home/v1" element={<HomePage />} />
+								<Route path="*" element={<Navigate to="/home" />} />
+							</Route>
+							<Route path="/login" element={<Authentication />} />
+						</Routes>
+					</Router>
+					<div className="mt-4">
+						<Footer />
 					</div>
-				</UserInfoProvider>
-			</SessionProvider>
+				</div>
+			</UserInfoProvider>
 		</div>
 	);
 }
