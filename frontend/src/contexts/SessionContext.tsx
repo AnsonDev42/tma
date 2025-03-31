ed102c9cd087b17d763257fc5ca9f1873ad85655
Yaowen.Shen <yaowen.shen@datalex.com>
2025-03-31 16:37:02 +0100

    remove extra supabase client

--- frontend/src/contexts/SessionContext.tsx
+++ frontend/src/contexts/SessionContext.tsx
@@ -1,12 +1,7 @@
+import supabase from "@/lib/supabaseClient.ts";
 import { AuthChangeEvent } from "@supabase/gotrue-js";
 import { Session } from "@supabase/gotrue-js/src/lib/types.ts";
-import { createClient } from "@supabase/supabase-js";
 import React, { ReactNode, createContext, useEffect, useState } from "react";
-
-const supabase = createClient(
-	"https://scwodhehztemzcpsofzy.supabase.co",
-	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNjd29kaGVoenRlbXpjcHNvZnp5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTUxMjA3MTksImV4cCI6MjAzMDY5NjcxOX0.j4O3aKoITFoJi36sQiPoh5PUWSDwwDDh02hhmMRF8HY",
-);
 
 interface SessionContextProps {
 	session: Session | null;
