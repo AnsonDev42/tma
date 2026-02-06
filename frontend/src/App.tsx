e2ad9e4eb4dba59444a316b9c5dd23b660616ebf
YW <58594437+AnsonDev42@users.noreply.github.com>
2026-02-06 20:10:31 +0000

    remove app landing page and use slider for sign-in

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,7 @@
 import "./globals.css";
-import { Authentication } from "@/components/features/Authentication/Authentication.tsx";
 import Footer from "@/components/ui/Footer.tsx";
 import { UserInfoProvider } from "@/contexts/UserInfoContext.tsx";
 import HomePageV2 from "@/pages/HomePageV2.tsx";
-import { ProtectedRoute } from "@/routers/router.tsx";
 import {
 	Navigate,
 	Route,
@@ -19,11 +17,9 @@ function App() {
 			<div className="min-h-screen">
 				<Router>
 					<Routes>
-						<Route element={<ProtectedRoute />}>
-							<Route path="/home" element={<HomePageV2 />} />
-							<Route path="*" element={<Navigate to="/home" />} />
-						</Route>
-						<Route path="/login" element={<Authentication />} />
+						<Route path="/home" element={<HomePageV2 />} />
+						<Route path="/login" element={<Navigate to="/home" replace />} />
+						<Route path="*" element={<Navigate to="/home" replace />} />
 					</Routes>
 				</Router>
 				<Footer />
